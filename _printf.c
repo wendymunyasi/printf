169d67b2d3cd3b905451ef46573176bd7135e362
wendy munyasi <wendymunyasi@gmail.com>
2022-04-18 21:24:33 +0300

    Add a trial and error func

--- _printf.c
+++ _printf.c
@@ -1,6 +1,7 @@
 /* Online C compiler to run C program online */
 #include <stdio.h>
 #include <stdarg.h>
+#include "main.h"
 
 /**
  * _printf - function that produces output according to a format.
@@ -21,7 +22,7 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	/* int i = 0;
 	va_list arg;
 
 	va_start(arg, format);
@@ -33,5 +34,40 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(arg);
+	return (1);
+	*/
+	va_list ap;
+	char *p, *sval;
+	int ival;
+	double dval;
+
+	va_start(ap, *format);
+	for (p = format; *p; p++)
+	{
+		if (*p != '%')
+		{
+			putchar(*p);
+			continue;
+		}
+		switch (*++p) 
+		{
+         	case 'd':
+            ival = va_arg(ap, int);
+            printf("%d", ival);
+            break;
+        case 'f':
+            dval = va_arg(ap, double);
+            printf("%f", dval);
+            break;
+        case 's':
+            for (sval = va_arg(ap, char *); *sval; sval++)
+            putchar(*sval);
+            break;
+        default:
+            putchar(*p);
+            break;
+      	}
+	}
+	va_end(ap); /* clean up when done */
 	return (1);
 }
